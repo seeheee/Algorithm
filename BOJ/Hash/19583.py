c65bf99c4072c57788ba82d83ddbe1d0a814949b
seeheee <53335160+seeheee@users.noreply.github.com>
2021-02-13 00:44:54 +0900

    Hash 마저 풀기

--- BOJ/Hash/19583.py
+++ BOJ/Hash/19583.py
@@ -1,2 +1,14 @@
-start, end, streaming = map(int, input().split())
+import sys
+start, end, streaming = map(str, input().split())
+start = int("".join(start.split(':')))
+end = int("".join(end.split(':')))
+streaming = int("".join(streaming.split(':')))
+
+dic = {}
+
+while(True):
+    line = sys.stdin.readline
+    if len(line) < 5:
+        break
+    time, name = map(str, line.split())
 
