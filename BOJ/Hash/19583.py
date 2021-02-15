bfa8639d6b877f7137c71e6e16e6f216432bae04
seeheee <53335160+seeheee@users.noreply.github.com>
2021-02-15 21:32:02 +0900

    hash 19583 해결

--- BOJ/Hash/19583.py
+++ BOJ/Hash/19583.py
@@ -5,10 +5,22 @@ end = int("".join(end.split(':')))
 streaming = int("".join(streaming.split(':')))
 
 dic = {}
-
+count = 0
 while(True):
-    line = sys.stdin.readline
+    # 얼만큼 입력으로 들어올지 모르니까는 반복문을 돌면서 readline으로 받다가 길이가 짧으면 탈출
+    line = sys.stdin.readline()
     if len(line) < 5:
         break
     time, name = map(str, line.split())
+    time = int("".join(time.split(':')))
+    if start >= time:
+        dic[name] = 1
+    elif end <= time <= streaming:
+        # 중복된 데이터가 들어와도 한번만 카운트를 하기 위해서 if문에 값이 1일 경우를 넣어줌
+        if dic.get(name) == 1:
+            dic[name] += 1
+            count += 1
+
+print(count)
+
 
