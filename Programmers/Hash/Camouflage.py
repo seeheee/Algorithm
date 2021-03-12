555b47fd6e739faca9a47dcb4db5e5934c9543ff
seeheee <53335160+seeheee@users.noreply.github.com>
2021-03-13 00:00:17 +0900

    프로그래머스 변장문제 출기

--- Programmers/Hash/Camouflage.py
+++ Programmers/Hash/Camouflage.py
@@ -1,1 +1,15 @@
 # 위장 문제 풀기
+
+def solution(clothes):
+    dic = {}
+    for i in clothes:
+        if i[1] in dic:
+            dic[i[1]] += 1
+        else:
+            dic[i[1]] = 1
+
+    answer = 1
+    for j in dic.values():
+        answer *= (j + 1)
+
+    return answer - 1
