f5e8b4992f0805a0dc60ee53b20bc251cfe54700
seeheee <ksh2001ksh@naver.com>
2021-04-08 16:55:55 +0900

    정렬 가장 큰 수 시간초과 해결

--- Programmers/sort/max.py
+++ Programmers/sort/max.py
@@ -9,3 +9,11 @@ def solution(numbers):
     result.sort(reverse=True)
 
     return result.pop(0)
+
+############ 시간초과 발생 해결 #############
+
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key= lambda x:x*3, reverse=True)
+    return str(int(''.join(numbers)))
