996aa96ddcede1588e75044c408a997b7d790df8
seeheee <ksh2001ksh@naver.com>
2021-05-05 00:53:43 +0900

    로봇청소기 완료

--- BOJ/BFS & DFS/14503.py
+++ BOJ/BFS & DFS/14503.py
@@ -6,37 +6,65 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 r, c, d = map(int, input().split())
 state = [list(map(int, input().split())) for _ in range(M)]
-visit = [[0]*N for _ in range(M)]
 
 que = deque()
 
+# 북 동 남 서 (0,1,2,3)
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def change(d):
+    # 북쪽일 경우 서쪽으로
+    if d == 0:
+        return 3
+    # 동쪽일 경우 북쪽
+    elif d == 1:
+        return 0
+    # 남쪽일 경우 동쪽
+    elif d == 2:
+        return 1
+    # 서쪽일 경우 남쪽
+    elif d == 3:
+        return 2
+
+def back(d):
+    # 북쪽이면 남쪽으로 한칸 후진
+    if d == 0:
+        return 2
+    # 서쪽->동쪽
+    elif d == 3:
+        return 1
+    # 남쪽 -> 북쪽
+    elif d == 2:
+        return 0
+    # 동쪽 -> 서쪽
+    elif d == 1:
+        return 3
+
 def bfs(x, y, dir):
     count = 1
-    que.append([x, y, d])
-    visit[x][y] = 1
+    que.append([x, y, dir])
+    state[x][y] = 2
     while que:
         x, y, dir = que.popleft()
-        if dir == 0:
-            for i in range(4):
-                nx = x + Ndx[i]
-                ny = y + Ndy[i]
-        elif dir == 1:
-            for i in range(4):
-                nx = x + Edx[i]
-                ny = y + Edy[i]
-        elif dir == 2:
-            for i in range(4):
-                nx = x + Sdx[i]
-                ny = y + Sdy[i]
-        elif dir == 3:
-            for i in range(4):
-                nx = x + Wdx[i]
-                ny = y + Wdx[i]
-                if 0 <= nx < M and 0 <= ny < N:
-                    visit[nx][ny] = 1
-                    que.append([nx, ny])
-                    count += 1
-    return count
+        temp = dir
+        for i in range(4):
+            temp = change(temp)
+            nx, ny = x + dx[temp], y + dy[temp]
+            if 0 <= nx < M and 0 <= ny < N and state[nx][ny] == 0:
+                state[nx][ny] = 2
+                que.append([nx, ny, temp])
+                count += 1
+                break
+
+            # 사방에 갈 곳이 없는 경우
+            if i == 3:
+                nx, ny = x + dx[back(dir)], y + dy[back(dir)]
+                que.append([nx, ny, dir])
+
+                # 뒷 칸도 벽인 경우
+                if state[nx][ny] == 1:
+                    return count
 
 
-bfs(c, r, d)
+print(bfs(r, c, d))
