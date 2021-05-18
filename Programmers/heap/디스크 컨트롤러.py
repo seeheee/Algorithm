9250472e54fac04a2149ff2853136b78f565492e
seeheee <ksh2001ksh@naver.com>
2021-05-18 18:34:17 +0900

    컨트롤러 문제 완료

--- Programmers/heap/디스크 컨트롤러.py
+++ Programmers/heap/디스크 컨트롤러.py
@@ -1,35 +1,38 @@
 import heapq
-import copy
 
-jobs = [[1, 6], [4, 3], [5, 2], [6, 1]]
-jobs_copy = copy.copy(jobs)
-time = 0
-
-heap1 = []
-sum1 = 0
-heap2 = []
+jobs = [[1, 6], [4, 3], [5, 2]]
 
 def solution(jobs):
-    time = 0
-    sum1 = 0
+    heap1 = []
+    now = 0
+    j = 0
+    start = -1
+    answer = 0
+    # 현재 시점과 총 대기 시간을 구한다.
+    # 현재시점: now
+    # 총 대기시점: answer
+    # 바로 이전에 완료한 작업의 시작시간: start
 
-    while heap1:
-        # 작업을 수행하고 있지 않을 때
-        if time == 0 or time < jobs[0][1]:
-            heapq.heapify(jobs)
-            result = heapq.heappop(jobs)
-            for i in result:
-                sum1 += i
-            time = sum1
-
-        # 작업을 수행하고 있을 때
-        else:
-            for i in range(len(jobs)):
+    while j < len(jobs):
+        for i in range(len(jobs)):
+            # 현재 수행중 가능한지 확인
+            if jobs[i][0] <= now and jobs[i][0] > start:
                 heapq.heappush(heap1, [jobs[i][1], jobs[i][0]])
-            result = heapq.heappop(heap1)
-            for i in result:
-                sum1 += i
-            time = sum1
+
+        # 힙이 있다는 것은 현재 시점에서 작업이 가능하다는 것
+        if heap1:
+            # current[1]이 소요시간 , current[0]이 바로 이전에 완료한 작업의 시작시간
+            current = heapq.heappop(heap1)
+            start = now
+            now = now + current[0]
+            answer = answer + (now - current[1])
+            # if문을 한번 돌면 작업 한 개 완료
+            j += 1
+        # 힙이 없다면 현재 시점에서 작업 불가능
+        else:
+            now += 1
+    return int(answer/len(jobs))
+
 
 
 
