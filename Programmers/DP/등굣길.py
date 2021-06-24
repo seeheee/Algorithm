1e5e2f49a89f8540d4e5b821581c938594cb8845
seeheee <ksh2001ksh@naver.com>
2021-06-24 16:15:46 +0900

    등굣길 완료 그러나 효율성 문제 해결하기

--- Programmers/DP/등굣길.py
+++ Programmers/DP/등굣길.py
@@ -1,23 +1,17 @@
 def solution(m, n, puddles):
     visited = [[0 for _ in range(m+1)]for _ in range(n+1)]
+
     # 1,1에서 출발 (0,0)이 아니다
     # 집 출발점
     visited[1][1] = 1
 
-    for i in range(1, n):
-        for j in range(1, m):
-            for k in puddles:
-                if [i, j] == k:
-                    visited[i][j] = 0
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if [j, i] in puddles:
+                visited[i][j] = 0
             else:
                 if i > 1:
                     visited[i][j] += visited[i-1][j]
                 if j > 1:
                     visited[i][j] += visited[i][j-1]
-    return visited[n-1][m-1]
-
-
-
-
-
-print(solution(4, 3, [[2, 2]]))
+    return visited[n][m]
