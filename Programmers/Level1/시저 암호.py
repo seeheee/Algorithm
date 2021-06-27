c62b790a65b0993ce3e5d45a7724e4d0d4565dff
seeheee <ksh2001ksh@naver.com>
2021-06-27 15:01:04 +0900

    시저암호와 행렬의 덧셈 해결완료

--- Programmers/Level1/시저 암호.py
+++ Programmers/Level1/시저 암호.py
@@ -1,15 +1,16 @@
+# 문자열 굳이 리스트에 담을 필요 없고 그냥 문자열 더하기 가능
+
 def solution(s, n):
     Alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    SAlpha = 'abcdefghijklmnopqrstuvwxyz'
-    A_list = list(map(str, Alpha))
-    S_list = list(map(str, SAlpha))
-    answer = []
-    for i in range(len(A_list)):
-        for j in s:
-            if j in A_list[i]:
-                answer.append(A_list[i+n])
-            elif j in S_list[i]:
-                answer.append(S_list[i+n])
-            elif j == ' ':
-                continue
+    SAlpha = 'abcdefghijklmnopqrstuvwxyz' # 25개의 원소 배열
+    answer = ''
+    for j in s:
+        if j in Alpha:
+            idx = Alpha.find(j)
+            answer += Alpha[(idx+n)%26]
+        elif j in SAlpha:
+            idx = SAlpha.find(j)
+            answer += SAlpha[(idx+n)%26]
+        elif j == ' ':
+            answer += ' '
     return answer
---
+++ Programmers/Level1/행렬의 덧셈.py
@@ -0,0 +1,4 @@
+def solution(arr1, arr2):
+    answer = [[i + j for i, j in zip(a1, a2)] for a1, a2 in zip(arr1, arr2)]
+    return answer
+
