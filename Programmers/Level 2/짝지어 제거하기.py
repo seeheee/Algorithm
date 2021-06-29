13306965f565684066372befc21fd0f40028f587
seeheee <ksh2001ksh@naver.com>
2021-06-29 12:23:02 +0900

    짝 지어 제거하기

---
+++ Programmers/Level 2/짝지어 제거하기.py
@@ -0,0 +1,40 @@
+# 효율성 통과 못 함
+def solution(s):
+    s_list = list(map(str, s))
+    stack = []
+    while s_list:
+        if stack: # 스택이 없는 경우를 만들어줘야 인덱스 오류 안남(맨 처음 부분이랑, s_list는 남았는데 스택이 비어있어 stack[-1]에서 오류남
+            if stack[-1] == s_list[0]:
+                s_list.pop(0)
+                stack.pop()
+            else:
+                stack.append(s_list.pop(0))
+        else:
+            stack.append(s_list.pop(0))
+
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
+
+
+
+# 효율성도 통과한 코드
+# s를 굳이 리스트로 넣을 필요가 없음
+def solution(s):
+    stack = []
+    for c in s:
+        if len(stack) == 0:
+            stack.append(c)
+        elif stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
+
+
+print(solution("cdcd"))
