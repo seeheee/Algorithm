20ece4d1494685756f85c477d3aca89db8a2a6e2
seeheee <ksh2001ksh@naver.com>
2021-07-10 17:39:37 +0900

    실패율 완료

--- Programmers/카카오/실패율.py
+++ Programmers/카카오/실패율.py
@@ -1,7 +1,5 @@
 def solution(N, stages):
     dic = {}
-    fail = 0
-    count = 0
     for i in range(1, N+2):
         dic[i] = 0
     for stage in stages:
@@ -10,33 +8,39 @@ def solution(N, stages):
         else:
             dic[stage] += 1
 
-
     dic = sorted(dic.items(), key=lambda x: x[0])
     print(dic)
+
     value_list = []
     for d in range(len(dic)):
         tmp = dic[d][1]
         value_list.append(tmp)
-    # print(value_list)
+    print(value_list)
 
     answer = []
-    for i in range(len(value_list)-1):
-        fail = value_list[i] / sum(value_list[i+1:])
-        # fail = value_list.pop(0) / sum(value_list)
-        answer.append(fail)
+    for i in range(len(value_list) - 1):
+        # 이 예외처리가 어려웠음(오류: ZeroDivisionError: division by zero)
+        # ex) 2/0
+        current = sum(value_list[i + 1:])
+        if current == 0:
+            answer.append(value_list[i])
+        else:
+            fail = value_list[i] / sum(value_list[i + 1:])
+            answer.append(fail)
 
     dic1 = {}
     for idx, v in enumerate(answer):
         dic1[idx+1] = v
 
     dic1 = sorted(dic1.items(), key=lambda x: x[1], reverse=True)
-    # print(dic1)
+    print(dic1)
 
-    ha = []
+    final = []
     for j in range(len(dic1)):
         tmp1 = dic1[j][0]
-        ha.append(tmp1)
-    return ha
+        final.append(tmp1)
+    return final
 
-print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+
+print(solution(4, [4,4,4,4,4]))
 
